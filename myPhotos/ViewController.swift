6dc01113cb44001c1bf2d18bd351ac8d5bafb0fe
student1 <student1@jh-mbair-158-06.d214.org>
2016-03-07 09:48:12 -0600

    Initial Commit

---
+++ myPhotos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myPhotos
+//
+//  Created by student1 on 3/7/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
